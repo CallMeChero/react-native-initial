8567dada6211b290eb4a4bd787b383c5ed5e0058
CallMeChero <david.ceranic@gmail.com>
2020-12-18 09:39:28 +0100

    Initial commit & creating reusable context

---
+++ src/context/BlogContext.js
@@ -0,0 +1,21 @@
+import React, { useReducer } from 'react';
+import CreateDataContext from './CreateDataContext';
+
+const blogReducer = (state,action) => {
+    switch (action.type) {
+        case 'add_blogpost':
+            return [...state, {title: `Blog Post #${state.length + 1}`}]
+    }
+};
+
+const addBlogPost = () => {
+    dispatch({
+        type: 'add_blogpost'
+    })
+};
+
+export const { Context, Provider } = CreateDataContext(
+    blogReducer, 
+    { addBlogPost },
+    []
+);
